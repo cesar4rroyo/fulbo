511126a6998ff1e5dd159a8402864738900c130f
Antonius Yonathan <atomicbomber.email@gmail.com>
2020-08-06 22:32:11 +0700

    Update.

--- app/TempatPenyewaan.php
+++ app/TempatPenyewaan.php
@@ -5,9 +5,12 @@ namespace App;
 use App\QueryBuilder\TempatPenyewaanBuilder;
 use Carbon\Carbon;
 use Carbon\CarbonInterval;
+use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
 
 /**
  * @property boolean terverifikasi
@@ -43,23 +46,22 @@ class TempatPenyewaan extends Model
             return [];
         }
 
-        $openingTime = Carbon::createFromFormat("H:i:s", $this->waktu_buka);
-        $closingTime = Carbon::createFromFormat("H:i:s", $this->waktu_tutup);
-        $sessionLength = CarbonInterval::createFromFormat("H:i:s", $this->panjang_sesi);
+        $serviceOpenPeriod = CarbonPeriod::between(
+            Carbon::createFromFormat("H:i:s", $this->waktu_buka),
+            Carbon::createFromFormat("H:i:s", $this->waktu_tutup),
+        )->setDateInterval(
+            CarbonInterval::createFromFormat("H:i:s", $this->panjang_sesi)
+        );
 
+        return LazyCollection::make(function () use($serviceOpenPeriod) {
+                $previous = $serviceOpenPeriod->current();
+                $serviceOpenPeriod->next();
 
-//        $result = [];
-//        for ($time = $openingTime; $time->lessThan($closingTime); $time = $time->add($sessionLength) ) {
-//            dump($time);
-//        }
-
-
-//
-//        dump(compact(
-//            "openingTime",
-//            "closingTime",
-//            "sessionLength"
-//        ));
-
+                while ($current = $serviceOpenPeriod->current()) {
+                    yield CarbonPeriod::between($previous, $current);
+                    $previous = $current;
+                    $serviceOpenPeriod->next();
+                }
+            })->all();
     }
 }
